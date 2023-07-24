8fde229890edc046b9cb732157f91ec513e093c5
guvenkarabulut <guven.07.568@gmail.com>
2023-07-24 17:11:28 +0300

    add new control_flow expressions

--- rust-learning/src/control_flow.rs
+++ rust-learning/src/control_flow.rs
@@ -6,3 +6,39 @@ pub(crate) fn if_expressions(){
         println!("condition false");
     }
 }
+
+pub(crate) fn if_else_expression(number:i32){
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+}
+pub(crate) fn one_line_if_else(){
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+}
+
+pub(crate) fn basic_loop(){
+    loop {
+        println!("again!");
+    }
+}
+
+pub(crate)fn returning_values_from_loop(){
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+}
--- rust-learning/src/main.rs
+++ rust-learning/src/main.rs
@@ -4,6 +4,8 @@ mod guess_number;
 mod control_flow;
 
 fn main() {
-    control_flow::if_expressions();
+    control_flow::one_line_if_else();
+//    control_flow::basic_loop();
+    control_flow::returning_values_from_loop();
 }
 
